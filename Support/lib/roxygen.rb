ecdd2bb3bf3be4f8bc7b1640016e753c00d00348
Tim Bates <timothy.c.bates@gmail.com>
2026-09-13 19:54:52 +1200

    Update roxygen.rb

--- Support/lib/roxygen.rb
+++ Support/lib/roxygen.rb
@@ -31,15 +31,15 @@ module Roxygen
 			require ENV["TM_SUPPORT_PATH"] + "/lib/escape.rb"
 
       line = ENV["TM_CURRENT_LINE"].to_s.chomp
-       # TM_LINE_INDEX is UTF-8 bytes; Ruby 2 indexes by character.
-       n = ENV["TM_LINE_INDEX"].to_i
-       index = if n <= 0
-           0
-       elsif n >= line.bytesize
-           line.length
-       else
-           line.byteslice(0, n).length
-       end
+      # TM_LINE_INDEX is UTF-8 bytes; Ruby 2 indexes by character.
+      n = ENV["TM_LINE_INDEX"].to_i
+      index = if n <= 0
+          0
+      elsif n >= line.bytesize
+          line.length
+      else
+          line.byteslice(0, n).length
+      end
       wrap_col = (ENV["TM_COLUMNS"] || ENV["TM_WRAP_COLUMN"] || 80).to_i
 
 			m = prefix_match(line)
